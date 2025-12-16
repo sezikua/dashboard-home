efd68ed1b92a6f5a093a572a14015faad5616dc2
sezikua <sezikua@gmail.com>
2025-12-16 14:21:51 +0200

    Use icon.svg as primary browser favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,14 +17,6 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
         url: "/icon.svg",
         type: "image/svg+xml",
       },
